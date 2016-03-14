91bb5e171532f550f9155576f7574c48de21b2b0
Steve <sk@sklise.com>
2016-03-14 17:54:40 -0400

    Set leiningen v 2
    ugh, dependencies
    
    Change procfile
    
    Procfile

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject true-cause "0.1.0-SNAPSHOT"
+(defproject true-cause "0.1.0"
   :description "FIXME: write description"
+  :min-lein-version "2.0.0"
   :url "http://true-cause-i-read-it-on-the-internet.net"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.4.0"]
@@ -9,9 +10,8 @@
                  [org.clojure/java.jdbc "0.3.7"]
                  [hiccup "1.0.5"]
                  [org.slf4j/slf4j-nop "1.7.2"]
-                 [clj-bonecp-url "0.1.1"]]
+                 [clj-bonecp-url "0.1.1"]
+                 [javax.servlet/servlet-api "2.5"]]
   :plugins [[lein-ring "0.9.7"]]
-  :profiles
-    {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}
-     :uberjar {:aot :all}}
+  :profiles {:uberjar {:aot :all}}
   :ring {:handler true-cause.core/app})
